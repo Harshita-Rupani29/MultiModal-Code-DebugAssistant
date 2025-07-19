645d75f065e006f2181eb54df1e1f33dafe28668
Harshita Rupani <harshitarupani711@gmail.com>
2025-07-19 20:45:40 +0530

    pages added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LandingPage from "./Pages/LandingPage";
+import AboutPage from "./Pages/AboutPage";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage/>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
