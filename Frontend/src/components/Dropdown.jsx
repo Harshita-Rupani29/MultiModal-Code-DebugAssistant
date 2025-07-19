12c580a8086a3e2c595db5b86fddb6581f6265f2
Harshita Rupani <harshitarupani711@gmail.com>
2025-07-19 20:33:41 +0530

    donee dropdownww

--- Frontend/src/components/Dropdown.jsx
+++ Frontend/src/components/Dropdown.jsx
@@ -26,9 +26,28 @@ const Dropdown = ({ language, handleLanguageChange }) => {
                 type="button"
             >
                 {language} {/* Display selected language and version */}
-                
+                <svg 
+                    className="w-2.5 h-2.5 ms-3 ml-2" 
+                    aria-hidden="true" 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" 
+                    viewBox="0 0 10 6"
+                >
+                    <path 
+                        stroke="currentColor" 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        strokeWidth="2" 
+                        d="m1 1 4 4 4-4" 
+                    />
+                </svg>
             </button>
             {isOpen && (
+                <div 
+                    id="dropdown" 
+                    className="z-10 absolute left-0 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-56 dark:bg-gray-700"
+                >
+                    <ul 
                         className="py-2 text-sm text-gray-700 dark:text-gray-200" 
                         aria-labelledby="dropdownDefaultButton"
                     >
